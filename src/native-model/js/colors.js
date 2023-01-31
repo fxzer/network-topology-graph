75f073e7944e3acd6c41085c25e8de0de2d4b8df
FXJ <fanxj@syscloud.cn>
2023-01-31 15:00:53 +0800

    🚀 原生JS网络拓扑图雏形

---
+++ src/native-model/js/colors.js
@@ -0,0 +1,44 @@
+export const lightColors = [
+  '#8FE9FF',
+  '#87EAEF',
+  '#FFC9E3',
+  '#A7C2FF',
+  '#FFA1E3',
+  '#FFE269',
+  '#BFCFEE',
+  '#FFA0C5',
+  '#D5FF86',
+];
+export const darkColors = [
+  '#7DA8FF',
+  '#44E6C1',
+  '#FF68A7',
+  '#7F86FF',
+  '#AE6CFF',
+  '#FF5A34',
+  '#5D7092',
+  '#FF6565',
+  '#6BFFDE',
+];
+export const uLightColors = [
+  '#CFF6FF',
+  '#BCFCFF',
+  '#FFECF5',
+  '#ECFBFF',
+  '#EAD9FF',
+  '#FFF8DA',
+  '#DCE2EE',
+  '#FFE7F0',
+  '#EEFFCE',
+];
+export const uDarkColors = [
+  '#CADBFF',
+  '#A9FFEB',
+  '#FFC4DD',
+  '#CACDFF',
+  '#FFD4F2',
+  '#FFD3C9',
+  '#EBF2FF',
+  '#FFCBCB',
+  '#CAFFF3',
+];
